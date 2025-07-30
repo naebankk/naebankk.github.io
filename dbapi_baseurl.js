83ab1f93911dceb636bfbc712230da97ec0ece70
VladosNX <72348449+VladosNX@users.noreply.github.com>
2025-07-30 21:20:14 +0700

    Update dbapi_baseurl.js

--- dbapi_baseurl.js
+++ dbapi_baseurl.js
@@ -1,1 +1,1 @@
-export const dbapi_baseurl = 'https://thingproxy.freeboard.io/fetch/http://45.67.202.82:5010';
+export const dbapi_baseurl = 'https://thingproxy.freeboard.io/fetch/http://45.67.202.82:5000';
